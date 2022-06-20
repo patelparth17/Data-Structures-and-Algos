1e7364058bcc0a4f749ae5f08b9b61be79a3db39
patelparth17 <parthdp2605@gmail.com>
2022-06-20 19:10:56 +0530

    Provided comments

--- Backtracking and Recursion/Combination Sum 1.cpp
+++ Backtracking and Recursion/Combination Sum 1.cpp
@@ -6,20 +6,20 @@ public:
         {
             if(target==0)
             {
-                ans.push_back(ds);
+                ans.push_back(ds);  //print the data structure as it contains all the elements of the target.
             }
             return;
         }
         
         if(candidates[ind]<=target)
         {
-            ds.push_back(candidates[ind]);
-            fun(ind,n,ds,ans,candidates,target-candidates[ind]);
-            ds.pop_back();
+            ds.push_back(candidates[ind]);      //This can be a potential member as it's value is less than target, push it into a data structure.
+            fun(ind,n,ds,ans,candidates,target-candidates[ind]);    //Repetition : As repetition is allowed, call for the same index again.
+            ds.pop_back();                      //As this value is processes alongwith it's children, pop it from the data structure.
         }
+
         
-        
-        fun(ind+1,n,ds,ans,candidates,target);
+        fun(ind+1,n,ds,ans,candidates,target);      //No repettition : Now look for those indices which do not contain the index element 
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
